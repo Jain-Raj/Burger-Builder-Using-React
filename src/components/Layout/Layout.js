dae0ba26be4d4dcbc0a9023f72b984230a387719
Raj <jainraj170@gmail.com>
2020-06-03 15:12:52 +0530

    Added responsive SideDrawer for mobile devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Auxiliary from '../../hoc/Auxiliary'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => (
     <Auxiliary>
         <Toolbar></Toolbar>
+        <SideDrawer></SideDrawer>
         <main className={classes.Content}>{props.children}</main>
     </Auxiliary>
 )
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import classes from './Logo.module.css'
 import burgerImage from '../../assets/images/burger-logo.png'
 
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo} style={{height: props.height}}>
         <img src={burgerImage} alt="My Burger"></img>
     </div>
 )
