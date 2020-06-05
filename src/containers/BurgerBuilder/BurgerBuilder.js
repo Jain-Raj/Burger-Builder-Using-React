703256a8cf4e49bf1cd327b311b48e5ca6dd5963
Raj <jainraj170@gmail.com>
2020-06-05 21:16:21 +0530

    Added spinner while sending HTTP requests

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import axios from '../../axios-orders'
+import Spinner from '../../components/UI/Spinner/Spinner'
 
 const INGREDIENTS_PRICE = {
     salad: 0.3,
@@ -22,7 +23,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         burgerPrice: 4,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     closemodalHandler = () => {
@@ -34,29 +36,30 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
+        this.setState({ loading: true })
+
         const orderInfo = {
             ingredients: this.state.burgerIngredients,
             burgerPrice: this.state.burgerPrice,
-            customerInfo:{
+            customerInfo: {
                 name: 'RJ',
-                address:{
+                address: {
                     street: 'Test Street',
                     zipCode: '530020',
                     country: 'India'
                 },
-                email:'test@test.com'
+                email: 'test@test.com'
 
             },
-            deliveryMEthod:'fastest'
+            deliveryMEthod: 'fastest'
         }
         axios.post('/orders.json', orderInfo)
-        .then((response) => {
-            console.log(response)
-        })
-        .catch((error) => {
-            console.log('RJ')
-            console.log(error)
-        })
+            .then((response) => {
+                this.setState({ loading: false, purchasing: false })
+            })
+            .catch((error) => {
+                this.setState({ loading: false, purchasing: false })
+            })
     }
 
     updateBurgerIngredients = (ingredientType, operationType) => {
@@ -76,15 +79,20 @@ class BurgerBuilder extends Component {
     }
 
     render() {
+        let orderSummary = <OrderSummary
+            ingredients={this.state.burgerIngredients}
+            purchaseCancel={this.closemodalHandler}
+            purchaseContinue={this.purchaseContinueHandler}
+            burgerPrice={this.state.burgerPrice} />
+
+        if (this.state.loading)
+            orderSummary = <Spinner />
+
         return (
             <Auxiliary>
                 {this.state.purchasing ?
                     <Modal modalClosed={this.closemodalHandler}>
-                        <OrderSummary
-                            ingredients={this.state.burgerIngredients}
-                            purchaseCancel={this.closemodalHandler}
-                            purchaseContinue={this.purchaseContinueHandler} 
-                            burgerPrice={this.state.burgerPrice}/>
+                        {orderSummary}
                     </Modal> : null
                 }
                 <Burger ingredients={this.state.burgerIngredients}></Burger>
