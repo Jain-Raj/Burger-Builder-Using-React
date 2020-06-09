61c9d1d848413a697fbd3431f9fe7d1c9d14d149
Raj <jainraj170@gmail.com>
2020-06-09 13:48:11 +0530

    Enabling Routing and setting up Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import CheckOut from './containers/CheckOut/CheckOut'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <BurgerBuilder></BurgerBuilder>
-        <CheckOut></CheckOut>
+        <Route path="/checkout" component={CheckOut}></Route>
+        <Route path="/" exact component={BurgerBuilder}></Route>
       </Layout>
     )
   }
